7b704216e9293eca01d7507822a453c444ddf4e9
shivendra <shivendra.bind@outlook.com>
2019-12-31 00:21:19 +0530

    simple blog detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import list_view
+from .views import list_view, detail_view
 
 app_name = 'blog'
 
 urlpatterns = [
     path('all', list_view, name='list'),
+    path('<int:pk>', detail_view , name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -8,4 +8,9 @@ class PostListView(ListView):
     template_name = 'list_view.html'
 
 
+class PostDetailView(DetailView):
+    model = Post
+
+
 list_view = PostListView.as_view()
+detail_view = PostDetailView.as_view()
